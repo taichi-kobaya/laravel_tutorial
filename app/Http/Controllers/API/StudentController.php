6aa279b301c8d9a5f3658309c980d4cb47610801
taichi-kobaya <taichi.kobayashi@eeeeg.tokyo>
2021-02-18 16:34:21 +0900

    API練習問題

---
+++ app/Http/Controllers/API/StudentController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            // デバッグしたいとき
+            Log::debug($id);
+            $student = Student::find($id);
+            $result = [
+                'result' => true,
+                'id'     => $student->id,
+                'name'   => $student->name,
+                'email'  => $student->email,
+                'tel'    => $student->tel
+            ];
+        } catch (\Exception $e) {
+            $result = [
+                'result' => false,
+                'error' => [
+                    'messages' => [$e->getMessage()]
+                ],
+            ];
+            return $this->resConversionJson($result, $e->getCode());
+        }
+        return response()->json($result, 200, [
+            'Content-Type' => 'application/json'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    private function resConversionJson($result, $statusCode = 200)
+    {
+        if (empty($statusCode) || $statusCode < 100 || $statusCode >= 600) {
+            $statusCode = 500;
+        }
+        return response()->json($result, $statusCode, [
+            'Content-Type' => 'application/json'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+/*localhost/api/studentというルーティングを生成*/
+Route::group(['middleware' => ['api']], function() {
+    /*リソースメソッドはGET,POST,PATCH,DELETE等でよく使うルーティングをまとめて生成してくれる*/
+    Route::resource('student', StudentController::class);
 });
